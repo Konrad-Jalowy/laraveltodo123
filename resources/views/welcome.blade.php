19358264ebd9eacf17cb28d7137030d48cb80e0f
Konrad-Jalowy <sitesystem@wp.pl>
2023-02-14 09:36:11 +0100

    basic functionality

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,40 @@
         @vite(['resources/js/app.js']) 
     </head>
     <body>
-        <button class="btn btn-primary">primary</button>
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">Task Name</th>
+        <th scope="col">Done</th>
+        <th scope="col">Actions</th>
+        </tr>
+    </thead>
+  <tbody>
+    
+    @forelse ($tasks as $task )
+    <tr>
+      <td>{{$task->task}}</td>
+      @if($task->done ===0)
+      <td>No</td>
+      @else
+      <td>Yes</td>
+      @endif
+      <td>
+      <button class="btn btn-primary">Mark as done</button>
+      <button class="btn btn-warning">Edit</button>
+      <button class="btn btn-success">Delete</button>
+      </td>
+      </tr>
+    @empty
+    <tr>
+        <td>No tasks available</td>
+        <td>N/A</td>
+        <td>N/A</td>
+    </tr>
+    @endforelse
+      
+    
+  </tbody>
+</table>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Task;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::all();
+    return view('welcome', compact('tasks'));
 });
